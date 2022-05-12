f09a841fbd55d317cf6753ae559ae48ca04f3cc3
Mikhail <mishajudoist@gmail.com>
2022-05-12 17:17:33 +0300

    done 1 part

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,9 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+
+	for _, elem := range input {
+		output = string(elem) + output
+	}
 	return output
 }
